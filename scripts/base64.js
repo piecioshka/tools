b60a3471d6af18348c7d295b461f0d522fa97c91
Piotr Kowalski <piecioshka@gmail.com>
2019-07-21 16:23:50 +0200

    Build Base64 => Text converter

--- scripts/base64.js
+++ scripts/base64.js
@@ -1,10 +1,21 @@
-function setupComponent({ input, output, button }) {
+function setupComponent({ input, output, button, convert }) {
     const $input = document.querySelector(input);
     const $output = document.querySelector(output);
     const $button = document.querySelector(button);
 
     $button.addEventListener('click', () => {
-        $output.value = btoa($input.value);
+        $output.value = convert($input.value);
+    });
+
+    $output.addEventListener('click', () => {
+        $output.selectionStart = 0;
+        $output.selectionEnd = $output.value.length;
+    });
+
+    $output.addEventListener('keydown', (evt) => {
+        if (evt.metaKey) return;
+        if (evt.ctrlKey) return;
+        evt.preventDefault();
     });
 }
 
@@ -12,8 +23,15 @@ function main() {
     setupComponent({
         input: '#text-base64-input',
         output: '#text-base64-output',
-        button: '#text-base64-convert'
+        button: '#text-base64-convert',
+        convert: btoa
+    });
+    setupComponent({
+        input: '#base64-text-input',
+        output: '#base64-text-output',
+        button: '#base64-text-convert',
+        convert: atob
     });
 }
 
-main();
+window.addEventListener('load', main);
